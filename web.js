00a5dc61df07f951ab8f2192a2d9d69127027477
andrew <supprlyapp@gmail.com>
2013-09-10 03:32:09 -0400

    added new css router

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 var content = fs.readFileSync('index.html', 'utf8');
+app.use('/public',express.static(__dirname + '/public'));
 app.get('/', function(request, response) {
   response.send(content);
 });
